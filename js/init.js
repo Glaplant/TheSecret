1ce83407858cc6cffd01ce727bc20852bd9d8b81
Greg LaPlant <glaplantwd@gmail.com>
2024-03-08 07:32:18 -0600

    Initial Commit

--- js/init.js
+++ js/init.js
@@ -1,10 +1,11 @@
-import {getPoems,getImages} from "./fetch.js"
-import {imageGrid} from "./buildlHTML.js"
+import { getPoems, getImages } from "./fetch.js";
+import { imageGrid } from "./buildlHTML.js";
 
-async function init(){
-    await getPoems();
-    await getImages();
-    await imageGrid();
+async function init() {
+	await getPoems();
+	console.log(getPoems);
+	await getImages();
+	await imageGrid();
 }
 
 init();
